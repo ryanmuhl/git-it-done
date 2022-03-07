186e4456a35ee3ac187309510f92546932c59d63
ryanmuhl <ryanmuhl@hotmail.com>
2022-03-07 17:31:48 -0500

    update homepage.js

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,20 +1,40 @@
-fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-  response.json().then(function(data) {
-    console.log(data);
-  });
+fetch("https://api.github.com/users/octocat/repos").then(function (response) {
+    response.json().then(function (data) {
+        console.log(data);
+    });
 });
-  
 
-var getUserRepos  = function(user) {
+
+var getUserRepos = function (user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
-  
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-      response.json().then(function(data) {
-        console.log(data);
-      });
-    });
-  };
 
-  getUserRepos ("ryanmuhl")
+    // make a request to the url
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
+};
+
+
+
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    console.log(event);
+
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
